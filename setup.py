6458f4438bbfb1b251f2b553514bd692e634d9dc
Sejal Mahajan <sejalmahajan2005@example.com>
2026-02-16 14:35:22 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Sejal-Mahajan",
+    author_email="sejalmahajan2005@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)                   
